591ceb3653032e62f14ebddd8bf2fb49cdcf2dbd
tnfssc <29162020+tnfssc@users.noreply.github.com>
2026-06-03 13:31:18 +0530

    Revert "feat(expt1): add liquid simulation component and internal experiment route"
    This reverts commit 34b0af663b5e1ea537001a61854e75f29916eb31.

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -21,7 +21,6 @@ import { Route as BlogSlugUpdateRouteImport } from './routes/blog/$slug/update'
 import { Route as ApiTrpcSplatRouteImport } from './routes/api/trpc/$'
 import { Route as ApiPosthogSplatRouteImport } from './routes/api/posthog/$'
 import { Route as ApiAuthSplatRouteImport } from './routes/api/auth/$'
-import { Route as Char43internalExpt001RouteImport } from './routes/+internal/expt/001'
 
 const UploadToCdnRoute = UploadToCdnRouteImport.update({
   id: '/upload-to-cdn',
@@ -83,11 +82,6 @@ const ApiAuthSplatRoute = ApiAuthSplatRouteImport.update({
   path: '/api/auth/$',
   getParentRoute: () => rootRouteImport,
 } as any)
-const Char43internalExpt001Route = Char43internalExpt001RouteImport.update({
-  id: '/+internal/expt/001',
-  path: '/+internal/expt/001',
-  getParentRoute: () => rootRouteImport,
-} as any)
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
@@ -97,7 +91,6 @@ export interface FileRoutesByFullPath {
   '/blog/create': typeof BlogCreateRoute
   '/blog/create-author': typeof BlogCreateAuthorRoute
   '/blog': typeof BlogIndexRoute
-  '/+internal/expt/001': typeof Char43internalExpt001Route
   '/api/auth/$': typeof ApiAuthSplatRoute
   '/api/posthog/$': typeof ApiPosthogSplatRoute
   '/api/trpc/$': typeof ApiTrpcSplatRoute
@@ -112,7 +105,6 @@ export interface FileRoutesByTo {
   '/blog/create': typeof BlogCreateRoute
   '/blog/create-author': typeof BlogCreateAuthorRoute
   '/blog': typeof BlogIndexRoute
-  '/+internal/expt/001': typeof Char43internalExpt001Route
   '/api/auth/$': typeof ApiAuthSplatRoute
   '/api/posthog/$': typeof ApiPosthogSplatRoute
   '/api/trpc/$': typeof ApiTrpcSplatRoute
@@ -128,7 +120,6 @@ export interface FileRoutesById {
   '/blog/create': typeof BlogCreateRoute
   '/blog/create-author': typeof BlogCreateAuthorRoute
   '/blog/': typeof BlogIndexRoute
-  '/+internal/expt/001': typeof Char43internalExpt001Route
   '/api/auth/$': typeof ApiAuthSplatRoute
   '/api/posthog/$': typeof ApiPosthogSplatRoute
   '/api/trpc/$': typeof ApiTrpcSplatRoute
@@ -145,7 +136,6 @@ export interface FileRouteTypes {
     | '/blog/create'
     | '/blog/create-author'
     | '/blog'
-    | '/+internal/expt/001'
     | '/api/auth/$'
     | '/api/posthog/$'
     | '/api/trpc/$'
@@ -160,7 +150,6 @@ export interface FileRouteTypes {
     | '/blog/create'
     | '/blog/create-author'
     | '/blog'
-    | '/+internal/expt/001'
     | '/api/auth/$'
     | '/api/posthog/$'
     | '/api/trpc/$'
@@ -175,7 +164,6 @@ export interface FileRouteTypes {
     | '/blog/create'
     | '/blog/create-author'
     | '/blog/'
-    | '/+internal/expt/001'
     | '/api/auth/$'
     | '/api/posthog/$'
     | '/api/trpc/$'
@@ -191,7 +179,6 @@ export interface RootRouteChildren {
   BlogCreateRoute: typeof BlogCreateRoute
   BlogCreateAuthorRoute: typeof BlogCreateAuthorRoute
   BlogIndexRoute: typeof BlogIndexRoute
-  Char43internalExpt001Route: typeof Char43internalExpt001Route
   ApiAuthSplatRoute: typeof ApiAuthSplatRoute
   ApiPosthogSplatRoute: typeof ApiPosthogSplatRoute
   ApiTrpcSplatRoute: typeof ApiTrpcSplatRoute
@@ -285,13 +272,6 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof ApiAuthSplatRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/+internal/expt/001': {
-      id: '/+internal/expt/001'
-      path: '/+internal/expt/001'
-      fullPath: '/+internal/expt/001'
-      preLoaderRoute: typeof Char43internalExpt001RouteImport
-      parentRoute: typeof rootRouteImport
-    }
   }
 }
 
@@ -303,7 +283,6 @@ const rootRouteChildren: RootRouteChildren = {
   BlogCreateRoute: BlogCreateRoute,
   BlogCreateAuthorRoute: BlogCreateAuthorRoute,
   BlogIndexRoute: BlogIndexRoute,
-  Char43internalExpt001Route: Char43internalExpt001Route,
   ApiAuthSplatRoute: ApiAuthSplatRoute,
   ApiPosthogSplatRoute: ApiPosthogSplatRoute,
   ApiTrpcSplatRoute: ApiTrpcSplatRoute,
